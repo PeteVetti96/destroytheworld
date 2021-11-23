528807d57e4906f4f3e785b38a46a4f88eeff906
Peters-MacBook-Pro <corvettipeter@gmail.com>
2021-11-23 12:43:02 -0500

    Initial Commit

---
+++ DestroyTheWorld/DestroyTheWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  DestroyTheWorldApp.swift
+//  DestroyTheWorld
+//
+//  Created by Peter Corvetti on 11/23/21.
+//
+
+import SwiftUI
+
+@main
+struct DestroyTheWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
